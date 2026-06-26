bc118bad6ba5066568c7f4409010335edfe91270
RahulInsane <137199975+RahulInsane@users.noreply.github.com>
2026-06-26 23:49:22 +0530

    Time: 96 ms (34.39%), Space: 165.3 MB (33.3%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-typedef pair<int,pair<int,int>> p;
+typedef pair<int,pair<int,int>>p;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<p,vector<p>> pq;
-        
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                int sum=nums1[i]+nums2[j];
-                if(pq.size()<k){
-                    pq.push({sum,{i,j}});
-                }
-                else if(pq.top().first>sum){
-                    pq.pop();
-                    pq.push({sum,{i,j}});
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        vector<vector<int>> result;
-        while(!pq.empty()){
+        int m=nums1.size();
+        int n=nums2.size();
+        priority_queue<p,vector<p>,greater<p>> pq;
+        int sum=nums1[0]+nums2[0];
+        set<pair<int,int>> visited;
+        pq.push({sum,{0,0}});
+        visited.insert({0,0});
+        vector<vector<int>> res;
+        while(k-- && !pq.empty()){
             auto temp=pq.top();
             pq.pop();
-            int i=temp.second.first;
-            int j= temp.second.second;
-            result.push_back({nums1[i],nums2[j]});
+            int i= temp.second.first;
+            int j=temp.second.second;
+            res.push_back({nums1[i],nums2[j]});
+            if(j+1<n && visited.find({i,j+1})==visited.end()){
+                pq.push({nums1[i]+nums2[j+1],{i,j+1}});
+                visited.insert({i,j+1});
+            }
+            if(i+1<m && visited.find({i+1,j})==visited.end()){
+                pq.push({nums1[i+1]+nums2[j],{i+1,j}});
+                visited.insert({i+1,j});
+            }
+
         }
-        return result;
+        return res;
+
     }
 };
