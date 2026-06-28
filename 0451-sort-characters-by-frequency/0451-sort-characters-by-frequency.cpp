08dcfc62963ff2616ae0e4e144001f23a6f7dce8
RahulInsane <137199975+RahulInsane@users.noreply.github.com>
2026-06-28 23:38:05 +0530

    Time: 5 ms (28.99%), Space: 10.9 MB (80.35%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        string ans="";
-        int freq[256]={0};
-        for(int i=0;i<s.size();i++){
-            freq[s[i]]++;
+        priority_queue<pair<int,char>> maxh;
+        unordered_map<char,int>mpp;
+        string res="";
+        for(int i=0;i<s.size();i++) mpp[s[i]]++;
+        for(auto it=mpp.begin();it!=mpp.end();it++){
+            maxh.push({it->second,it->first});
         }
-        vector<pair<int,char>> v;
-        for(int i=0;i<256;i++){
-            if(freq[i]>0) v.push_back({freq[i],char(i)});
-
+        while(!maxh.empty()){
+            char el=maxh.top().second;
+            int freq= maxh.top().first;
+            for(int i=1;i<=freq;i++) res+=el;
+            maxh.pop();
         }
-        sort(v.begin(),v.end(),greater<pair<int,char>>());
-        for(int i=0;i<v.size();i++){
-            int count=v[i].first;
-            char ch=v[i].second;
-            while(count>0){
-                ans+=ch;
-                count--;
-            }
-        }
-        return ans;
+        return res;
     }
 };
