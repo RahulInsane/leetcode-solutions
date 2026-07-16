72ef43a0537261c3a663ec657e57a22a5fd94718
RahulInsane <137199975+RahulInsane@users.noreply.github.com>
2026-07-16 23:52:43 +0530

    Time: 3 ms (33.49%), Space: 69.6 MB (29.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -7,7 +7,7 @@ public:
         int maxlen=0;
         while(r<nums.size()){
             if(nums[r]==0) zeros++;
-            while(zeros>k){
+            if(zeros>k){
                 if(nums[l]==0) zeros--;
                 l++;
             }
