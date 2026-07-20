9e1e0b4cbd7466862b5773866473234cbea0affa
RahulInsane <137199975+RahulInsane@users.noreply.github.com>
2026-07-20 22:44:02 +0530

    Time: 4 ms (21.09%), Space: 42.1 MB (34.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    vector<int> calculate_prefix(int n, vector<int>&nums){
-        vector<int>res(n);
+    vector<int> prefix_product(int n, vector<int>&nums){
         int product=1;
+        vector<int> res(n);
         for(int i=0;i<n;i++){
             res[i]=product;
-            product=product*nums[i];
-            
+            product*=nums[i];
         }
         return res;
     }
-    vector<int>calculate_suffix(int n, vector<int>&nums){
-        vector<int>res(n);
+    vector<int> suffix_product(int n, vector<int> &nums){
         int product=1;
+        vector<int> res(n);
         for(int i=n-1;i>=0;i--){
             res[i]=product;
             product*=nums[i];
@@ -21,11 +20,12 @@ public:
     }
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prefix_product=calculate_prefix(n,nums);
-        vector<int> suffix_product=calculate_suffix(n,nums);
+        vector<int> prefix= prefix_product(n,nums);
+        vector<int> suffix=suffix_product(n,nums);
         vector<int> ans(n);
         for(int i=0;i<n;i++){
-            ans[i]=prefix_product[i]*suffix_product[i];
+            ans[i]=prefix[i]*suffix[i];
+
         }
         return ans;
     }
